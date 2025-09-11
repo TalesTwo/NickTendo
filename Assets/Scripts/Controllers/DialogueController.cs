f86ef782e1fd9aadac22f6e7c07df3b64d2a8362
Noah Vincent <noahvincent62@yahoo.ca>
2025-09-11 15:21:59 -0600

    Part three of last commit (added comments to code)

--- Assets/Scripts/Controllers/DialogueController.cs
+++ Assets/Scripts/Controllers/DialogueController.cs
@@ -5,25 +5,31 @@ using UnityEngine.UI;
 
 public class DialogueController : MonoBehaviour
 {
+    // dialogue objects
     public GameObject dialogueBox;
     public Text dialogueText;
     public string[] dialogue;
     
     private int index;
 
+    // optional wordspeed for if we want the text to display at a certain speed
     public float wordSpeed;
+    
+    // tracking if the player is in range to engage the dialogue
     public bool playerIsClose;
     
+    // checking if the player can continue the dialogue (line must finish first)
     private bool canContinue = false;
     
     void Start()
     {
-        dialogueText.text = "";
+        dialogueText.text = ""; // fixes a bug later in the code (do not touch)
     }
 
     // Update is called once per frame
     void Update()
     {
+        // engage dialogue box
         if (Input.GetKeyDown(KeyCode.E) && playerIsClose)
         {
             if (dialogueBox.activeInHierarchy)
@@ -37,17 +43,20 @@ public class DialogueController : MonoBehaviour
             }
         }
 
+        // move to next line of dialogue
         if (Input.GetKeyDown(KeyCode.Space) && canContinue)
         {
             NextLine();
         }
 
+        // checking if current line of dialogue is finished
         if (dialogueText.text == dialogue[index])
         {
             canContinue = true;
         }
     }
 
+    // resets the dialogue box
     public void ZeroText()
     {
         dialogueText.text = "";
@@ -55,6 +64,7 @@ public class DialogueController : MonoBehaviour
         dialogueBox.SetActive(false);
     }
 
+    // types each letter in the dialogue one at a time
     IEnumerator Typing()
     {
         foreach (char letter in dialogue[index].ToCharArray())
@@ -64,6 +74,7 @@ public class DialogueController : MonoBehaviour
         }
     }
 
+    // advances to next line of dialogue
     public void NextLine()
     {
         canContinue = false;
@@ -80,6 +91,7 @@ public class DialogueController : MonoBehaviour
         }
     }
 
+    // check if player enters dialogue area
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -88,6 +100,7 @@ public class DialogueController : MonoBehaviour
         }
     }
 
+    // check if player leaves dialogue area
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // player speed
     public float speed;
     
+    // input variables
     public float horizontalInput;
     public float verticalInput;
 
+    // rigidbody
     private Rigidbody2D rb;
     
     // Start is called before the first frame update
@@ -16,20 +19,20 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
     }
-
+    
     private void Update()
     {
+        // get WASD input
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
     }
 
     // Update is called once per frame
+    // "fixedDeltaTime" is necessary instead of "Delta Time" for this method
     private void FixedUpdate()
     {
+        // move player based on input
         Vector2 update = new Vector2(horizontalInput, verticalInput);
-        
         rb.MovePosition(rb.position + update * speed * Time.fixedDeltaTime);
-
-        //transform.Translate(new Vector3(horizontalInput * speed * Time.deltaTime, verticalInput * speed * Time.deltaTime, 0));
     }
 }
