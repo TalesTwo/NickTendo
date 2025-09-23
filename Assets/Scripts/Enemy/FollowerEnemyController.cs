056773ab5149173701bda17c05de55a8240f3efd
Noah Vincent <noahvincent62@yahoo.ca>
2025-09-23 10:01:32 -0600

    added FollowerEnemy movmement, knockback, and hit flash

---
+++ Assets/Scripts/Enemy/FollowerEnemyController.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowerEnemyController : MonoBehaviour
+{
+    public int health = 10;
+    public float speed = 2;
+    public float knockBackSpeed = 10;
+    public float knockBackTime = 0.15f;
+    public float hitFlashDuration = 0.1f;
+
+    private Rigidbody2D _rb;
+    private GameObject _player;
+    private SpriteRenderer _renderer;
+    private Color _color;
+    
+    private bool _isKnockback = false;
+
+    private Vector3 _direction;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _renderer = GetComponent<SpriteRenderer>();
+        _color = _renderer.color;
+        _player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // step 1: check death condition
+        CheckForDeath();
+        
+        // step 2: get movement direction
+        _direction = GetDirection();
+        
+        // step 3: check for knockback then move: direction dependent on knockback state
+        if (!_isKnockback)
+        {
+            transform.Translate(_direction * (speed * Time.deltaTime), Space.World);
+        }
+    }
+
+    // reduce health on damage from a PlayerAttack
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("DashAttack"))
+        {
+            health -= (int)PlayerStats.Instance.GetDashDamage();
+            SetKnockBack();
+        } else if (collision.gameObject.CompareTag("PlayerAttack"))
+        {
+            health -= (int)PlayerStats.Instance.GetAttackDamage();
+            SetKnockBack();
+        }
+    }
+
+    // find the intended direction of movement
+    private Vector3 GetDirection()
+    {
+        return (_player.transform.position - transform.position).normalized;
+    }
+
+    // check for death condition
+    private void CheckForDeath()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // todo 
+    // add particles
+    private void SetKnockBack()
+    {
+        _isKnockback = true;
+        _rb.AddForce(new Vector2(-_direction.x, -_direction.y) * knockBackSpeed, ForceMode2D.Impulse);
+        StartCoroutine(HitFlash());
+        Invoke(nameof(ResetKnockBack), knockBackTime);
+    }
+
+    // reset knockback state
+    private void ResetKnockBack()
+    {
+        _isKnockback = false;
+    }
+
+    // flash the sprite white to indicate a hit
+    IEnumerator HitFlash()
+    {
+        _renderer.color = Color.white;
+        yield return new WaitForSeconds(hitFlashDuration);
+        _renderer.color = _color;
+    }
+}
---
+++ Assets/Scripts/Enemy/RangedEnemyController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedEnemyController : MonoBehaviour
+{
+    public int health = 10;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // reduce health on damage from a PlayerAttack
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("DashAttack"))
+        {
+            health -= (int)PlayerStats.Instance.GetDashDamage();
+        } else if (collision.gameObject.CompareTag("PlayerAttack"))
+        {
+            health -= (int)PlayerStats.Instance.GetAttackDamage();
+        }
+    }
+}
