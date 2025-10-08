eadba2e101f17c40e126edd87e13c9e96b39deaa
Pix-L-Boi-ehhhh2125 <pxvanbussel@gmail.com>
2025-10-08 12:33:09 -0600

    Working Soundtracks

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -12,6 +12,7 @@ namespace Managers
         public void Start()
         {
             // Temporaryily start the main menu music here
+            AudioManager.Instance.PlayOverworldTrack(1f, true, 0.1f, true, 0.1f);
             var playerController = GameObject.FindGameObjectWithTag("Player")?.GetComponent<PlayerController>();
             if (playerController != null)
             {
@@ -22,6 +23,7 @@ namespace Managers
         {
             if (_usernameInputField && !string.IsNullOrWhiteSpace(_usernameInputField.text))
             {
+                AudioManager.Instance.PlayOverworldTrack(1f, true, 0.1f, true, 0.1f);
                 PlayerStats.Instance.SetPlayerName(_usernameInputField.text);
                 StartGame();
             }
