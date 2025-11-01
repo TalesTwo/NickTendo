5de2a7afacd63dbd61c943d6e98ebb75d289cc14
Noah <nvincent@ualberta.ca>
2025-11-01 11:37:12 -0600

    added controller script to boss

---
+++ Assets/Scripts/Boss/BossArmController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * This script controls how the arms interact with the world
+ *
+ * This script is responsible for:
+ * * controlling the arms general movement
+ * * controlling the arms movement during rocket mode (called by boss controller)
+ * * controlling the arms movement during swing mode (called by boss controller)
+ * * damaging the player on contact
+ *
+ * * calling for the hands to perform actions (if the hands are animated)
+ */
+public class BossArmController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Boss/BossController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * The central hub for controlling all things boss fight
+ *
+ * This script is responsible for:
+ * * initiating the arm attacks
+ * * spawning minions
+ * * shooting projectiles
+ * * managing health
+ * * calling for animations
+ * * calling for the arms to perform actions
+ * * managing vulnerable and invulnerable stages
+ * * managing the state of the battle
+ * * pushing the player back when attacked during invulnerable mode
+ */
+public class BossController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
