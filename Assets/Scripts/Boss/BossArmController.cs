f6cdcda1797590b36706f5ab5f46550b089eaf29
Noah <nvincent@ualberta.ca>
2025-11-01 13:52:41 -0600

    Boss arms can detach

--- Assets/Scripts/Boss/BossArmController.cs
+++ Assets/Scripts/Boss/BossArmController.cs
@@ -28,9 +28,38 @@ public class BossArmController : MonoBehaviour
     }
     
     public Direction side;
+    public int DirectionModifier;
+
+    public float offScreenXCoordinate = 30f;
+    private Vector2 _velocity = Vector2.zero;
+    public float smoothDampTime = 0.2f;
+    public float moveSpeedAttack = 10f;
     
-    private void Start()
+    private void Start() {
+        if (side == Direction.Right)
+        {
+            DirectionModifier = 1;
+        }
+        else
+        {
+            DirectionModifier = -1;
+        }
+    }
+    
+    public void LaunchAttack()
     {
-        
+        Vector2 destination = new Vector2(offScreenXCoordinate * DirectionModifier, transform.position.y);
+        StartCoroutine(MoveArm(destination));
+    }
+
+    private IEnumerator MoveArm(Vector2 destination)
+    {
+        while (true)
+        {
+            //transform.position = Vector2.MoveTowards(transform.position, destination, moveSpeedSafe * Time.deltaTime);
+            //transform.position = Vector3.Lerp(transform.position, destination,  Time.deltaTime * moveSpeedSafe);
+            transform.position = Vector2.SmoothDamp(transform.position, destination, ref _velocity, smoothDampTime);
+            yield return null;            
+        }
     }
 }
--- Assets/Scripts/Boss/BossController.cs
+++ Assets/Scripts/Boss/BossController.cs
@@ -19,7 +19,9 @@ public class BossController : MonoBehaviour
 {
     [Header("body parts")]
     public GameObject rightArm;
+    public BossArmController rightArmController;
     public GameObject leftArm;
+    public BossArmController leftArmController;
     public GameObject face;
 
     public enum HealthState
@@ -59,6 +61,26 @@ public class BossController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LaunchLeftArm();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            LaunchRightArm();
+        }
+    }
+
+    // launching the left arm for rocket attack
+    private void LaunchLeftArm()
+    {
+        leftArmController.LaunchAttack();
+    }
+
+    // launching the right arm for rocket attack
+    private void LaunchRightArm()
+    {
+        rightArmController.LaunchAttack();
     }
 }
