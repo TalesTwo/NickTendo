a91e4d1c2584aeeb5074e3d4bda7e7c9c1b262d5
Noah <nvincent@ualberta.ca>
2025-11-28 20:02:36 -0700

    added some new boss animations

---
+++ Assets/Scripts/Animators/AnimatedBossShoulder.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedBossShoulder : AnimatedEntity
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AnimationSetup();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimationUpdate();
+    }
+}
