29d771fefc209355d3df158562b07d32d5f003be
Noah <nvincent@ualberta.ca>
2025-12-04 14:08:42 -0700

    Fixed Rocket Arm speed glitch, also fixed Red screen background not appearing error

--- Assets/Scripts/Boss/AnimatedBossFace.cs
+++ Assets/Scripts/Boss/AnimatedBossFace.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class AnimatedBossFace : AnimatedEntity
 {
+    public SpriteRenderer screen;
+    public Sprite redScreen;
+    public Sprite blueScreen;
+    public float redScreenTime;
     public List<Sprite> idleFrames;
     public List<Sprite> exhaustedFrames;
     public List<Sprite> loadingMinionsFrames;
@@ -34,6 +38,13 @@ public class AnimatedBossFace : AnimatedEntity
     public void SetLoadingMinionsAnimation()
     {
         Interrupt(loadingMinionsFrames);
+        screen.sprite = redScreen;
+        Invoke(nameof(RemoveRedScreen), redScreenTime);
+    }
+
+    public void RemoveRedScreen()
+    {
+        screen.sprite = blueScreen;
     }
 
     public void SetHurtAnimation()
